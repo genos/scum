d9add76dc0096c83f5256e22d8d8b6946327c7f4
Graham Enos <genos@rigetti.com>
2023-03-15 21:43:03 -0400

    nearly there for parsing

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 #[derive(Debug, thiserror::Error)]
 pub enum ScumError {
-
+    #[error("Parsing error: {0:#?}")]
+    ParsingError(#[from] nom::error::VerboseError<String>)
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,107 @@
+use crate::expression::{Expression, Identifier};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{anychar, char, digit1, multispace0},
+    combinator::{cut, map, map_res, verify},
+    error::{context, VerboseError},
+    multi::many0,
+    number::complete::double,
+    sequence::{delimited, pair, preceded},
+    IResult, Parser,
+};
+
+fn _conv_err(e: VerboseError<&str>) -> VerboseError<String> {
+    VerboseError {
+        errors: e
+            .errors
+            .into_iter()
+            .map(|(s, k)| (s.to_string(), k))
+            .collect(),
+    }
+}
+
+type ParseResult<'a, T = Expression> = IResult<&'a str, T, VerboseError<&'a str>>;
+
+fn _paren<'a, T>(
+    inner: impl Parser<&'a str, T, VerboseError<&'a str>>,
+) -> impl FnMut(&'a str) -> ParseResult<T> {
+    delimited(
+        char('('),
+        preceded(multispace0, inner),
+        cut(preceded(multispace0, char(')'))),
+    )
+}
+
+fn _is_protected(x: &char, xs: &[char]) -> bool {
+    *x == '#' && (xs == vec!['t'] || xs == vec!['f'])
+}
+
+fn _identifier(input: &str) -> ParseResult<Identifier> {
+    context(
+        "id",
+        map(
+            verify(
+                pair(
+                    context(
+                        "id first",
+                        verify(anychar, |c| !(c.is_numeric() || c.is_whitespace())),
+                    ),
+                    context("id rest", many0(verify(anychar, |c| !c.is_whitespace()))),
+                ),
+                |(x, xs)| !_is_protected(x, xs),
+            ),
+            |(x, xs)| Identifier(format!("{}{}", x, xs.iter().collect::<String>())),
+        ),
+    )(input)
+}
+
+fn _expression(input: &str) -> ParseResult {
+    context("expr", alt((_atom, _bool, _float, _int, _list)))(input)
+}
+
+fn _atom(input: &str) -> ParseResult {
+    context("atom", map(_identifier, Expression::Atom))(input)
+}
+
+fn _bool(input: &str) -> ParseResult {
+    context(
+        "bool",
+        map(alt((tag("#t"), tag("#f"))), |s| Expression::Bool(s == "#t")),
+    )(input)
+}
+
+fn _float(input: &str) -> ParseResult {
+    context("float", double.map(Expression::Float))(input)
+}
+
+fn _int(input: &str) -> ParseResult {
+    context(
+        "int",
+        alt((
+            map_res(digit1, |s: &str| s.parse::<i64>().map(Expression::Int)),
+            map(preceded(tag("-"), digit1), |s: &str| {
+                Expression::Int(-s.parse::<i64>().unwrap())
+            }),
+        )),
+    )(input)
+}
+
+fn _list(input: &str) -> ParseResult {
+    context("list", _paren(map(many0(_expression), Expression::List)))(input)
+}
+
 #[cfg(test)]
 mod test {
-    use crate::expression::{Expression, Identifier};
+    use super::*;
     use proptest::prelude::*;
 
     fn arb_identifier() -> impl Strategy<Value = Identifier> {
-        "[^\\d\\s]\\S*".prop_map(Identifier)
+        (
+            any::<char>().prop_filter("first", |c| !(c.is_numeric() || c.is_whitespace())),
+            any::<String>().prop_filter("rest", |s| !(s.chars().any(|c| c.is_whitespace()))),
+        )
+            .prop_map(|(c, s)| Identifier(format!("{c}{s}")))
     }
 
     fn arb_expression() -> impl Strategy<Value = Expression> {
@@ -28,7 +125,6 @@ mod test {
     }
 
     proptest! {
-
         #[test]
         fn arb_id_ok(i in arb_identifier()) {
             prop_assert_eq!(i.clone(), i.clone())
@@ -39,5 +135,24 @@ mod test {
             prop_assert_eq!(exp.clone(), exp.clone())
         }
 
+        #[test]
+        fn id_round_trip(i in arb_identifier()) {
+            let s = i.clone().to_string();
+            let p = _identifier(&s);
+            prop_assert!(p.is_ok());
+            let (rest, i2) = p.unwrap();
+            prop_assert_eq!(rest, "");
+            prop_assert_eq!(i2, i);
+        }
+
+        #[test]
+        fn expr_round_trip(exp in arb_expression()) {
+            let s = exp.clone().to_string();
+            let p = _expression(&s);
+            prop_assert!(p.is_ok());
+            let (rest, exp2) = p.unwrap();
+            prop_assert_eq!(rest, "");
+            prop_assert_eq!(exp2, exp);
+        }
     }
 }
