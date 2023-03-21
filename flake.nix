90e9a283161ed4ddbc827d95fcdb651fbbabd98c
Graham <genos@users.noreply.github.com>
2023-03-21 16:17:40 -0400

    more nix (#7)

--- flake.nix
+++ flake.nix
@@ -5,9 +5,14 @@
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
-    naersk = { 
+    naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
     nixpkgs.url = "github:nixos/nixpkgs";
   };
@@ -16,19 +21,36 @@
     self,
     flake-utils,
     naersk,
+    rust-overlay,
     nixpkgs,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [rust-overlay.overlays.default];
+      };
       naersk' = pkgs.callPackage naersk {};
+      buildInputs =
+        []
+        ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+        ];
+      buildPackage = pname:
+        naersk'.buildPackage {
+          inherit buildInputs pname;
+          src = ./.;
+        };
+      scum-lib = buildPackage "scum-lib";
+      scum-repl = buildPackage "scum-repl";
     in {
       formatter = pkgs.alejandra;
-      defaultPackage = naersk'.buildPackage {
-        name = "scum";
-        src = ./.;
+      packages = {
+        inherit scum-lib scum-repl;
+        default = scum-repl;
       };
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [pkgs.rustc pkgs.cargo pkgs.libiconv];
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.rustc pkgs.cargo];
+        inherit buildInputs;
       };
     });
 }
