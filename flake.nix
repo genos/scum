a81ebe05ee3809d79446f051a4fe8d25693ccf28
Graham Enos <graham@strangeworks.com>
2024-08-27 16:24:44 -0400

    squish nix

--- flake.nix
+++ flake.nix
@@ -17,51 +17,43 @@
     };
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    rust-overlay,
-    nixpkgs,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs@{ flake-parts, rust-overlay, nixpkgs, naersk, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = {
-        self',
-        system,
-        ...
-      }: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.libiconv];
-        naersk = pkgs.callPackage inputs.naersk {};
-        buildCrate = pkg:
-          naersk.buildPackage {
-            inherit nativeBuildInputs pkg;
-            src = ./.;
+      perSystem = { self', system, ... }:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          toolchain =
+            pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          nativeBuildInputs =
+            pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+          buildCrate = pkg:
+            (pkgs.callPackage naersk {
+              cargo = toolchain;
+              rustc = toolchain;
+            }).buildPackage {
+              inherit nativeBuildInputs pkg;
+              src = ./.;
+            };
+        in {
+          formatter = pkgs.nixfmt;
+          packages = rec {
+            scum-lib = buildCrate "scum-lib";
+            scum-repl = buildCrate "scum-repl";
+            default = scum-repl;
           };
-      in {
-        formatter = pkgs.alejandra;
-        packages = rec {
-          scum-lib = buildCrate "scum-lib";
-          scum-repl = buildCrate "scum-repl";
-          default = scum-repl;
-        };
-        apps = rec {
-          scum-repl = {
-            type = "app";
-            program = "${self'.packages.scum-repl}/bin/scum-repl";
+          apps = rec {
+            scum-repl = {
+              type = "app";
+              program = "${self'.packages.scum-repl}/bin/scum-repl";
+            };
+            default = scum-repl;
           };
-          default = scum-repl;
+          devShells.default = pkgs.mkShell {
+            inherit nativeBuildInputs;
+            buildInputs = [ toolchain ];
+          };
         };
-        devShells.default = pkgs.mkShell {
-          inherit nativeBuildInputs;
-          buildInputs = [
-            toolchain
-          ];
-        };
-      };
     };
 }
